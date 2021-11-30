a304c22d342d7eea572e5daf11e9816936119aac
wdjacca <chanjacqueline0418@gmail.com>
2021-11-29 22:52:47 -0500

    SOB side bar, XML mark up done

--- newtfire/showhideToggle.js
+++ newtfire/showhideToggle.js
@@ -1,12 +1,15 @@
 // JavaScript Document
-window.addEventListener('DOMContentLoaded',pageLoadUp,false);
+window.addEventListener('DOMContentLoaded',announ,false);
+window.addEventListener('DOMContentLoaded',SD,false);
+window.addEventListener('DOMContentLoaded',music,false);
+window.addEventListener('DOMContentLoaded',sound,false);
 
-function pageLoadUp() {
-document.getElementById('toggle').addEventListener('click', toggle, false)  
+function announ() {
+document.getElementById('announToggle').addEventListener('click', announToggle, false)  
     
 }
 
-var toggle = function() {  
+var announToggle = function() {  
     var spans = document.getElementsByClassName('announcer');
     for (var i = 0; i < spans.length; i++) {
      console.log('divs at position i is now: ' + spans[i] + "i is: " + i)
@@ -14,3 +17,46 @@ var toggle = function() {
        
     } 
 }
+
+function SD() {
+document.getElementById('stageToggle').addEventListener('click', SDtoggle, false)  
+    
+}
+
+var SDtoggle = function() {  
+    var spans = document.getElementsByClassName('stageDirect');
+    for (var i = 0; i < spans.length; i++) {
+     console.log('divs at position i is now: ' + spans[i] + "i is: " + i)
+     spans[i].classList.toggle("off")
+       
+    } 
+}
+
+function music() {
+document.getElementById('musicToggle').addEventListener('click', musicToggle, false)  
+    
+}
+
+var musicToggle = function() {  
+    var spans = document.getElementsByClassName('music');
+    for (var i = 0; i < spans.length; i++) {
+     console.log('divs at position i is now: ' + spans[i] + "i is: " + i)
+     spans[i].classList.toggle("off")
+       
+    } 
+}
+
+function sound() {
+document.getElementById('soundToggle').addEventListener('click', soundToggle, false)  
+    
+}
+
+var soundToggle = function() {  
+    var spans = document.getElementsByClassName('sound');
+    for (var i = 0; i < spans.length; i++) {
+     console.log('divs at position i is now: ' + spans[i] + "i is: " + i)
+     spans[i].classList.toggle("off")
+       
+    } 
+}
+
